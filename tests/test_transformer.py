5d8ef89220c16d63ff1ab7bca033c4606a183984
Mukund Biradar <mukund2015@outlook.com>
2025-03-20 18:08:39 +0100

    Update test_transformer.py

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,1 +1,21 @@
 
+import pytest
+from src.transformer import transform_record
+
+def test_transform_record():
+    record = {
+        "id": 1,
+        "name": "Test Facility",
+        "phone": "+123456789",
+        "url": "http://test.com",
+        "latitude": 40.7128,
+        "longitude": -74.0060,
+        "country": "US",
+        "locality": "New York",
+        "region": "NY",
+        "postal_code": "10001",
+        "street_address": "123 Test St"
+    }
+    transformed = transform_record(record)
+    assert transformed["name"] == "Test Facility"
+    assert transformed["location"]["latitude"] == 40.7128
