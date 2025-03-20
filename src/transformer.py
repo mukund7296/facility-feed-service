759d22d5b04017597f1cfa8944efaea6721defd7
Mukund Biradar <mukund2015@outlook.com>
2025-03-20 18:05:50 +0100

    Update transformer.py

--- src/transformer.py
+++ src/transformer.py
@@ -1,1 +1,20 @@
 
+def transform_record(record):
+    """Convert facility data to JSON format."""
+    return {
+        "entity_id": f"dining-{record['id']}",
+        "name": record["name"],
+        "telephone": record["phone"],
+        "url": record["url"],
+        "location": {
+            "latitude": record["latitude"],
+            "longitude": record["longitude"],
+            "address": {
+                "country": record["country"],
+                "locality": record["locality"],
+                "region": record["region"],
+                "postal_code": record["postal_code"],
+                "street_address": record["street_address"]
+            }
+        }
+    }
